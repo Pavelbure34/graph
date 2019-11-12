bd3046b19c8df5ed2da089dcd19c73cd9021d9b5
Dongbin Suh <dongbinsuh@gmail.com>
2019-11-11 23:54:36 -0500

    sketching

--- graph.cpp
+++ graph.cpp
@@ -4,3 +4,8 @@
     This is implementation for graph class.
     Coded by Alistaire and Youssef
 */
+
+template<class T> //constructor
+vertex<T>::vertex(T* item = NULL){
+    this->key = (item != NULL)?new T(*item):NULL;
+}
--- graph.h
+++ graph.h
@@ -1,9 +1,33 @@
 #ifndef _GRAPH_H
 #define _GRAPH_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 /*
     This is ADT for graph class.
     Coded by Alistaire and Youssef
 */
+
+template<class T>
+class vertex{
+public:
+    int id;                      //integer identifier
+    T* key;                      //key item
+    vector<vertex<T>*> inbound;  //for indegree
+    vector<vertex<T>*> outbound; //for outdegree
+
+    vertex(T* item = NULL);      //constructor
+};
+
+template<class T>
+class graph{
+private:
+    int size = 0;
+
+    graph();
+
+};
 
 #include "graph.cpp"
 #endif
